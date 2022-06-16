ff7d90783433fb00cf9fc6034db34003d9f89972
Thor <gallithor@gmail.com>
2022-06-16 13:38:21 -0300

    Added MouseMove Sript for player and Bouncy Physics material.

--- Assets/Scripts/MouseMove.cs
+++ Assets/Scripts/MouseMove.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseMove : MonoBehaviour
-{
+public class MouseMove : MonoBehaviour {
+
+    [SerializeField] protected float sensitivity;
+    private Rigidbody2D rigidBody;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate() {
+        if (Input.GetMouseButton(0)) {
+            rigidBody.drag = 15;
+            ControlPlayer();
+        } else {
+            rigidBody.drag = 0;
+        }
+    }
+
+    void ControlPlayer() {
+        Vector2 movement = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        rigidBody.AddForce(movement * sensitivity);
     }
 }
